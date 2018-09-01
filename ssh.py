dc1a6d53516529ea8f2d235b77d91e7c8e3dd2bb
lenovo-Recuer <775121173@qq.com>
2018-09-01 23:05:23 +0800

    终于可以使用python的ssh模块来快速开网,断网了

--- ssh.py
+++ ssh.py
@@ -3,17 +3,15 @@ import paramiko
 #创建ssh对象
 ssh = paramiko.SSHClient()
 
-hostname = ''
-username = ''
-password = ''
+
 
 #把主机添加到konwn_host
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 #连接到服务器
-ssh.connect(hostname=hostname,port=22,username=username,password=password)
+ssh.connect(hostname=hostname,port=22,username=username,password=password,allow_agent=False,look_for_keys=False)
 
-cmd = 'ps'
+cmd = 'sys'
 
 stdin, stdout,stderr = ssh.exec_command(cmd)
 
