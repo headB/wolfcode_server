11e5c834d666ebd92c52230801947856d648297b
lenovo-Recuer <775121173@qq.com>
2018-09-01 12:32:36 +0800

    为ssh做准备

---
+++ ssh.py
@@ -0,0 +1,27 @@
+import paramiko
+
+#创建ssh对象
+ssh = paramiko.SSHClient()
+
+hostname = ''
+username = ''
+password = ''
+
+#把主机添加到konwn_host
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+#连接到服务器
+ssh.connect(hostname=hostname,port=22,username=username,password=password)
+
+cmd = 'ps'
+
+stdin, stdout,stderr = ssh.exec_command(cmd)
+
+res = stdout.read()
+
+if not res:
+    res = stderr.read()
+
+ssh.close()
+
+print(res)
---
+++ views/postman/postman.py
@@ -0,0 +1,4 @@
+#这个模块,专用用于模拟发送的
+
+import requests
+
