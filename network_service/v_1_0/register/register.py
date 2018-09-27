1822ff8428c9c9c7076ad8ad8f3ed4d5114a95d0
asus-wolfcode-beetle <775121173@qq.com>
2018-09-27 11:30:54 +0800

    test

--- network_service/v_1_0/register/register.py
+++ network_service/v_1_0/register/register.py
@@ -1,24 +1,3 @@
-# 这个就专门将微信的openid绑定到公司邮箱的
-# 这个过程都是使用蓝图的
-
-#这里,设计两个地方
-#一个负责注册
-#另一个负责登录把.反正都是不负责的.
-
-#OK,
-#首先需要知道的是,让公司的员工的微信openid绑定公司邮箱,进行实名认证
-
-#1.openid
-#2.email
-#3.department
-#4.login_time
-
-#然后,权限可以另外设置权限表,先不着急,先设置
-
-#然后呢,注册的时候,这里就需要模拟提供信息给<评分系统>,因为<评分系统>是独立的.!
-
-#然后评分系统唯一需要改的地方就是,在登录验证的时候,去除验证码!
-
 from flask import render_template,request
 import datetime
 import time
@@ -72,6 +51,7 @@ def index():
         # timestamp = time.mktime(datetime.datetime.now().timetuple())
         timestamp = time.time()
         ##捕捉一些野生openid
+        
         return_content = decode_msg(content_xml)
         print(return_content)
         req_con = return_content['Content']
@@ -187,7 +167,7 @@ def index():
         return x1
 
     else:
-        
+
         return render_template('index.html')
 
 
